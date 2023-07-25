2a0c04d26e2399fbd393726332c3e521e78bd0fb
Your Name <you@example.com>
2023-07-26 00:13:27 +0300

    Create header and first screen

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        arrows: false,
+
+        autoplay: true,
+        autoplaySpeed: 4500,
+        speed: 1500,
+        slideToShow: 1,
+        swipe: true,
+    });
+  });
