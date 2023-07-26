675391ccc2692947c4fe12f2b7530a63f1128181
Your Name <you@example.com>
2023-07-26 23:57:48 +0300

    Finished desktop without map

--- js/slider.js
+++ js/slider.js
@@ -10,3 +10,33 @@ $(document).ready(function(){
         swipe: true,
     });
   });
+
+
+$(document).ready(function(){
+  $('.news__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: true
+  });
+});
+
+// var $wrapper = $('<div class="wrapper"></div>');
+
+// $('.item').on('click', function(event) {
+  
+//   var $clone = $(this).clone();
+//   $clone.addClass('wrapper__img');
+ 
+//   $wrapper.html($clone);
+//   $wrapper.appendTo('body').fadeIn(400); 
+  
+// });
+
+// $wrapper.on('click', function(event) {
+//     $(this).empty();
+//     $(this).fadeOut(400, function() {
+//       $(this).detach();
+//     });    
+//  });
